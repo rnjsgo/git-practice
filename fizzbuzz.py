130fd60f32a91a2ea52ab46a170a04fdb29c42b3
rnjsgo <rnjsgo2008@naver.com>
2023-04-24 21:36:10 +0900

    feat: Print fizz
    Condition: i is muliple of 3.
    Otherwise, print i itself.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,19 @@
-def do_fizzbuzz():
-	"""
-	fizzbuzz: print fizz buzz fizzbuzz
-	3: fizz
-	5: buzz
-	15: fizzbuzz
-	etc: num
-	"""
-	print('hello'
+def do_fizzbuzz(num:int):
+    """
+    fizzbuzz: print fizz buzz fizzbuzz
+    3: fizz
+    5: buzz
+    15: fizzbuzz
+    etc: num
+    """
+    for i in range(1,num+1):
+        if i%3==0:
+            print('fizz')
+        else:
+            print(f'{i}')
+
 
 if __name__=='__main__':
-	do_fizzbuzz()
+    do_fizzbuzz(16)
+
+
